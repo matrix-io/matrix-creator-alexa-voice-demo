f7ab124b30a3258dc2bf5120a33e57a3eccd390f
Kevin Patino <kdpatinos@unal.edu.co>
2016-08-10 17:28:26 +0000

    Adding Button Functionality

--- micarray/micarray_dump.cpp
+++ micarray/micarray_dump.cpp
@@ -8,11 +8,14 @@
 #include <string>
 #include <iostream>
 #include <valarray>
+#include <unistd.h>
 
 #include <matrix_hal/everloop_image.h>
 #include <matrix_hal/everloop.h>
 #include <matrix_hal/microphone_array.h>
 #include <matrix_hal/wishbone_bus.h>
+
+#define kIRRxPin 16
 
 namespace hal = matrix_hal;
 
@@ -23,20 +26,41 @@ int main() {
   hal::MicrophoneArray mics;
   mics.Setup(bus);
 
+  pinMode(kIRRxPin, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  digitalWrite(13, HIGH);
+  digitalWrite(5, HIGH);
+
   hal::Everloop everloop;
   everloop.Setup(bus);
 
   hal::EverloopImage image1d;
 
-  for (auto& led : image1d.leds) led.red = 10;
+  for (auto& led : image1d.leds) led.blue = 10;
 
   everloop.Write(&image1d);
 
-  uint16_t seconds_to_record = 4;
+  uint16_t seconds_to_record = 6;
 
   int16_t buffer[mics.Channels()][seconds_to_record * mics.SamplingRate()];
 
   uint32_t step = 0;
+
+  while (true) {
+    if (!digitalRead(16)) {
+      usleep(10);
+      if (!digitalRead(16)) break;
+    }
+  }
+
+  for (auto& led : image1d.leds) {
+    led.blue = 0;
+    led.red = 10;
+  }
+  everloop.Write(&image1d);
+
   while (true) {
     mics.Read(); /* Reading 8-mics buffer from de FPGA */
 
@@ -51,7 +75,7 @@ int main() {
 
   uint16_t c = 0;
   std::cout.write((const char*)buffer[c],
-                 seconds_to_record * mics.SamplingRate() * sizeof(int16_t));
+                  seconds_to_record * mics.SamplingRate() * sizeof(int16_t));
 
   std::cout.flush();
 
@@ -60,6 +84,8 @@ int main() {
     led.green = 10;
   }
   everloop.Write(&image1d);
-
+  // }
+  digitalWrite(13, LOW);
+  digitalWrite(5, LOW);
   return 0;
 }
